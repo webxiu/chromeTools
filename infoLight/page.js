5c595322d1059863fdbd683606909b373001185f
Hailen <erpit2@deogra.com>
2025-03-31 15:48:47 +0800

    fix: 动态html渲染

--- infoLight/page.js
+++ infoLight/page.js
@@ -41,8 +41,8 @@ function getField(str) {
   return str?.split(',').map(m => m.trim())
 }
 
-// 自动更新标题
-async function setTitle(data) {
+// 更新表格columns配置
+async function setTableColumns(data) {
   const { oneline, oneline2, hidden, hidden2, sortable, sortable2, content } = data
 
   const onelineFields = getField(oneline2) // 不换行的字段
@@ -55,12 +55,12 @@ async function setTitle(data) {
   }
 
   try {
-    var fieldWrap = dom(".designer-member.tree");// 成员列表
+    var fieldWrap = dom(".designer-member.tree");
     if (fieldWrap) {
       const topWrap = fieldWrap.parentNode.parentNode.parentNode;
       topWrap.id = "autoId";
       const fieldNodes = fieldWrap.querySelectorAll(".tree-node");
-      const list = getEle(fieldNodes).slice(0, 5);
+      const list = getEle(fieldNodes)
       for (let i = 0; i < list.length; i++) {
         const ele = list[i];
         const prop = ele.querySelector('span:nth-child(3)').innerText.split(' (')[0];
@@ -68,13 +68,13 @@ async function setTitle(data) {
         const filterFields = ['id'] // 不修改的字段
         printAligned(`prop: ${prop}`, item?.label || '==========字段不存在');
 
+
         // 获取属性类名
         function getClass(trIndex, tdIndex) {
-          const propTable = '#autoId .datagrid-view2 .datagrid-btable'
-          const className = `${propTable} tr:nth-child(${trIndex}) td:nth-child(${tdIndex}) div`
+          const tableClass = '#autoId .datagrid-view2 .datagrid-btable'
+          const className = `${tableClass} tr:nth-child(${trIndex}) td:nth-child(${tdIndex}) div`
           return className
         }
-
 
         if (!filterFields.includes(prop)) {
           ele.click();
@@ -82,27 +82,202 @@ async function setTitle(data) {
           const rowTitle = dom(getClass(1, 2));
           rowTitle.click();
           const input = rowTitle.querySelector('input')
-          if (input && item) input.value = item.label// 修改标题
-
-          // 设置属性
-          async function setProperty({ prop, fieldList, isCheck, trIndex, tdIndex }) {
-            if (fieldList.includes(prop) || !fieldList.length) {
-              const propDom = dom(getClass(trIndex, tdIndex));
-              propDom.click()
-              await sleep(301);
-              propDom.click()
-              const checkDom = propDom.querySelector('input[type="checkbox"]');
-              checkDom.checked = isCheck;
-            }
+          if (input && item) {
+            input.value = item.label
           }
 
           // 是否一行显示
-          setProperty({ prop, fieldList: onelineFields, isCheck: !!oneline, trIndex: 6, tdIndex: 2 })
+          if (onelineFields.includes(prop) || !onelineFields.length) {
+            const nowrapDom = dom(getClass(6, 2));
+            nowrapDom.click()
+            await sleep(301);
+            nowrapDom.click()
+            const nowrapCheckDom = nowrapDom.querySelector('input[type="checkbox"]');
+            nowrapCheckDom.checked = !!oneline;
+          }
           // 是否隐藏
-          setProperty({ prop, fieldList: hiddenFields, isCheck: !!hidden, trIndex: 7, tdIndex: 2 })
+          if (hiddenFields?.includes(prop) || !hiddenFields.length) {
+            const hiddenDom = dom(getClass(7, 2));
+            hiddenDom.click()
+            await sleep(301);
+            hiddenDom.click()
+            const hiddenCheckDom = hiddenDom.querySelector('input[type="checkbox"]');
+            hiddenCheckDom.checked = !!hidden;
+          }
           // 是否排序
-          setProperty({ prop, fieldList: sortableFields, isCheck: !!sortable, trIndex: 8, tdIndex: 2 })
+          if (sortableFields?.includes(prop) || !sortableFields?.length) {
+            const sortDom = dom(getClass(8, 2));
+            sortDom.click()
+            await sleep(301);
+            sortDom.click()
+            const sortCheckDom = sortDom.querySelector('input[type="checkbox"]');
+            sortCheckDom.checked = !!sortable;
+          }
+        }
+      }
+    }
+    console.log('执行完毕');
+    showMessage('执行完毕');
 
+  } catch (error) {
+    console.error('报错:', error);
+    return showMessage('数据格式错误', '#f60');
+  }
+}
+// 更新编辑columns配置
+async function setEditColumns(data) {
+  const { oneline, oneline2, hidden, hidden2, sortable, sortable2, content } = data
+  const onelineFields = getField(oneline2) // 不换行的字段
+  const hiddenFields = getField(hidden2) // 隐藏的字段
+  const sortableFields = getField(sortable2) // 排序的字段
+  const columns = JSON.parse(content);
+
+  if (!columns.length || !content) {
+    return showMessage('数据不能为空', '#f60');
+  }
+
+  try {
+    var fieldWrap = dom(".designer-member.tree");
+    if (fieldWrap) {
+      const topWrap = fieldWrap.parentNode.parentNode.parentNode;
+      topWrap.id = "autoId";
+      const fieldNodes = fieldWrap.querySelectorAll(".tree-node");
+      const list = getEle(fieldNodes)
+      for (let i = 0; i < list.length; i++) {
+        const ele = list[i];
+        const prop = ele.querySelector('span:nth-child(3)').innerText.split(' (')[0];
+        const item = columns.find(item => item.prop === prop)
+        const filterFields = ['id'] // 不修改的字段
+        printAligned(`prop: ${prop}`, item?.label || '==========字段不存在');
+
+
+        // 获取属性类名
+        function getClass(trIndex, tdIndex) {
+          const tableClass = '#autoId .datagrid-view2 .datagrid-btable'
+          const className = `${tableClass} tr:nth-child(${trIndex}) td:nth-child(${tdIndex}) div`
+          return className
+        }
+
+        if (!filterFields.includes(prop)) {
+          ele.click();
+          await sleep(300);
+          const rowTitle = dom(getClass(1, 2));
+          rowTitle.click();
+          const input = rowTitle.querySelector('input')
+          if (input && item) {
+            input.value = item.label
+          }
+
+          // 是否一行显示
+          if (onelineFields.includes(prop) || !onelineFields.length) {
+            const nowrapDom = dom(getClass(6, 2));
+            nowrapDom.click()
+            await sleep(301);
+            nowrapDom.click()
+            const nowrapCheckDom = nowrapDom.querySelector('input[type="checkbox"]');
+            nowrapCheckDom.checked = !!oneline;
+          }
+          // 是否隐藏
+          if (hiddenFields?.includes(prop) || !hiddenFields.length) {
+            const hiddenDom = dom(getClass(7, 2));
+            hiddenDom.click()
+            await sleep(301);
+            hiddenDom.click()
+            const hiddenCheckDom = hiddenDom.querySelector('input[type="checkbox"]');
+            hiddenCheckDom.checked = !!hidden;
+          }
+          // 是否排序
+          if (sortableFields?.includes(prop) || !sortableFields?.length) {
+            const sortDom = dom(getClass(8, 2));
+            sortDom.click()
+            await sleep(301);
+            sortDom.click()
+            const sortCheckDom = sortDom.querySelector('input[type="checkbox"]');
+            sortCheckDom.checked = !!sortable;
+          }
+        }
+      }
+    }
+    console.log('执行完毕');
+    showMessage('执行完毕');
+
+  } catch (error) {
+    console.error('报错:', error);
+    return showMessage('数据格式错误', '#f60');
+  }
+}
+// 更新枚举字典columns配置
+async function setDictionaryColumns(data) {
+  const { oneline, oneline2, hidden, hidden2, sortable, sortable2, content } = data
+
+  const onelineFields = getField(oneline2) // 不换行的字段
+  const hiddenFields = getField(hidden2) // 隐藏的字段
+  const sortableFields = getField(sortable2) // 排序的字段
+
+  const columns = JSON.parse(content);
+  if (!columns.length || !content) {
+    return showMessage('数据不能为空', '#f60');
+  }
+
+  try {
+    var fieldWrap = dom(".designer-member.tree");
+    if (fieldWrap) {
+      const topWrap = fieldWrap.parentNode.parentNode.parentNode;
+      topWrap.id = "autoId";
+      const fieldNodes = fieldWrap.querySelectorAll(".tree-node");
+      const list = getEle(fieldNodes)
+      for (let i = 0; i < list.length; i++) {
+        const ele = list[i];
+        const prop = ele.querySelector('span:nth-child(3)').innerText.split(' (')[0];
+        const item = columns.find(item => item.prop === prop)
+        const filterFields = ['id'] // 不修改的字段
+        printAligned(`prop: ${prop}`, item?.label || '==========字段不存在');
+
+
+        // 获取属性类名
+        function getClass(trIndex, tdIndex) {
+          const tableClass = '#autoId .datagrid-view2 .datagrid-btable'
+          const className = `${tableClass} tr:nth-child(${trIndex}) td:nth-child(${tdIndex}) div`
+          return className
+        }
+
+        if (!filterFields.includes(prop)) {
+          ele.click();
+          await sleep(300);
+          const rowTitle = dom(getClass(1, 2));
+          rowTitle.click();
+          const input = rowTitle.querySelector('input')
+          if (input && item) {
+            input.value = item.label
+          }
+
+          // 是否一行显示
+          if (onelineFields.includes(prop) || !onelineFields.length) {
+            const nowrapDom = dom(getClass(6, 2));
+            nowrapDom.click()
+            await sleep(301);
+            nowrapDom.click()
+            const nowrapCheckDom = nowrapDom.querySelector('input[type="checkbox"]');
+            nowrapCheckDom.checked = !!oneline;
+          }
+          // 是否隐藏
+          if (hiddenFields?.includes(prop) || !hiddenFields.length) {
+            const hiddenDom = dom(getClass(7, 2));
+            hiddenDom.click()
+            await sleep(301);
+            hiddenDom.click()
+            const hiddenCheckDom = hiddenDom.querySelector('input[type="checkbox"]');
+            hiddenCheckDom.checked = !!hidden;
+          }
+          // 是否排序
+          if (sortableFields?.includes(prop) || !sortableFields?.length) {
+            const sortDom = dom(getClass(8, 2));
+            sortDom.click()
+            await sleep(301);
+            sortDom.click()
+            const sortCheckDom = sortDom.querySelector('input[type="checkbox"]');
+            sortCheckDom.checked = !!sortable;
+          }
         }
       }
     }
@@ -116,11 +291,27 @@ async function setTitle(data) {
 }
 
 
-// 接收background.js中的数据
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  const { action, data, tabId } = message || {}
-  if (action !== "SET_TITLE") return
-  setTitle(data);
-  sendResponse({ success: true });
-});
 
+function addMessageListener() {
+  // 接收background.js中的数据
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    const { action, data, tabId } = message || {}
+    if (action !== "SET_TITLE") return
+    if (data.selectType === 'table') {
+      setTableColumns(data);
+    }
+    if (data.selectType === 'edit') {
+      setEditColumns(data);
+    }
+    if (data.selectType === 'dictionary') {
+      setDictionaryColumns(data);
+    }
+    sendResponse({ success: true });
+  });
+}
+
+// 确保只调用一次
+if (!window.messageListenerAdded) {
+  addMessageListener();
+  window.messageListenerAdded = true;
+}
--- infoLight/popup.js
+++ infoLight/popup.js
@@ -20,6 +20,8 @@ function setItem(data) {
 }
 
 function getDoms() {
+  const selectType = $("#selectType"); // 换行
+  const newLine = $("#newLine"); // 新行
   const oneline = $("#oneline"); // 换行
   const oneline2 = $("#oneline2");
   const hidden = $("#hidden"); // 隐藏
@@ -27,36 +29,87 @@ function getDoms() {
   const sortable = $("#sortable"); // 排序
   const sortable2 = $("#sortable2");
   const content = $("#content"); // 内容
-  return { oneline, oneline2, hidden, hidden2, sortable, sortable2, content }
+  return { selectType, newLine, oneline, oneline2, hidden, hidden2, sortable, sortable2, content }
 }
-function initData(storedData) {
+
+
+
+const selectList = [
+  { label: "表格配置", value: "table" },
+  { label: "编辑详情", value: "edit" },
+  { label: "数据字典", value: "dictionary" },
+]
+
+const configObj = {
+  table: [
+    { id: "oneline", value: "oneline2", label: "是否换行", type: "checkbox", placeholder: "请输入字段名 以逗号分开", },
+    { id: "hidden", value: "hidden2", label: "是否隐藏", type: "checkbox", placeholder: "请输入字段名 以逗号分开", },
+    { id: "sortable", value: "sortable2", label: "是否排序", type: "checkbox", placeholder: "请输入字段名 以逗号分开", },
+  ],
+  edit: [
+    { id: "newLine", value: "newLine2", label: "是否新行", type: "checkbox", placeholder: "请输入字段名 以逗号分开", },
+    { id: "hidden", value: "hidden2", label: "是否隐藏", type: "checkbox", placeholder: "请输入字段名 以逗号分开", },
+  ],
+  dictionary: []
+}
+
+function initData() {
+  const storedData = getItem(cache_key);
   const doms = getDoms()
-  doms.oneline.checked = storedData.oneline ?? '';
-  doms.oneline2.value = storedData.oneline2 ?? '';
-  doms.hidden.checked = storedData.hidden ?? '';
-  doms.hidden2.value = storedData.hidden2 ?? '';
-  doms.sortable.checked = storedData.sortable ?? '';
-  doms.sortable2.value = storedData.sortable2 ?? '';
-  doms.content.value = storedData.content ?? '';
+  Object.keys(storedData).forEach((key) => {
+    const domElement = doms[key];
+    if (domElement && !['selectType'].includes(key)) {
+      if (['oneline', 'hidden', 'sortable', 'newLine'].includes(key)) {
+        domElement.checked = storedData[key] ?? '';
+      } else {
+        domElement.value = storedData[key] ?? '';
+      }
+    }
+  })
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+  const wrapDom = document.querySelector(".wrap-box");
   const settingDom = document.getElementById("setting");
-  const stopDom = document.getElementById("stop");
-  const storedData = getItem(cache_key);
-
-  initData(storedData)// 初始化本地数据
+  const selectTypeDom = document.querySelector("#selectType");
+  const selectHtml = selectList.map((item) => `<option value="${item.value}">${item.label}</option>`).join("");
+  selectTypeDom.innerHTML = selectHtml
+  function render(ev) {
+    const value = ev?.target?.value ?? "table";
+    const htmls = configObj[value].map((item) => {
+      return `<div class="flex">
+          <label for="${item.id}" class="no-wrap">
+            ${item.label}<input id="${item.id}" type="${item.type}" />
+          </label>
+          <textarea
+            name="${item.value}"
+            id="${item.value}"
+            value="#aaaa,#bbbb"
+            rows="1"
+            class="w-100"
+            placeholder="${item.placeholder}"
+          ></textarea>
+        </div>
+       `
+    }).join("");
+    wrapDom.innerHTML = htmls
+    initData()
+  }
+  render();
+  selectTypeDom.addEventListener("change", render)
 
   settingDom.addEventListener("click", function () {
-    const { oneline, oneline2, hidden, hidden2, sortable, sortable2, content } = getDoms()
+    const { selectType, newLine, oneline, oneline2, hidden, hidden2, sortable, sortable2, content } = getDoms()
     const formData = {
-      oneline: oneline.checked,
-      oneline2: oneline2.value,
-      hidden: hidden.checked,
-      hidden2: hidden2.value,
-      sortable: sortable.checked,
-      sortable2: sortable2.value,
-      content: content.value
+      selectType: selectType?.value,
+      newLine: newLine?.checked,
+      oneline: oneline?.checked,
+      oneline2: oneline2?.value,
+      hidden: hidden?.checked,
+      hidden2: hidden2?.value,
+      sortable: sortable?.checked,
+      sortable2: sortable2?.value,
+      content: content?.value
     };
     setItem(formData);// 更新本地数据
 
@@ -75,21 +128,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  stopDom.addEventListener("click", function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.runtime.sendMessage({
-        action: 'stop_event',
-        data: '停止',
-        tabId: tabs[0].id
-      }, (response) => {
-        if (chrome.runtime.lastError) {
-          console.error(chrome.runtime.lastError.message);
-        } else {
-          console.log('Message sent successfully:', response);
-        }
-      });
-    });
-  });
 });
 
 
